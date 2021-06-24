08593238e10b6933ecd372d6e595dc2579443c2e
rajarora-taboola <rajendra.a@taboola.com>
2021-06-24 06:15:31 +0530

    Added changes

---
+++ scripts/plugin-configs.js
@@ -0,0 +1,9 @@
+(function() {
+  // Required for Taboola JS
+  // window._taboola = window._taboola || [];
+
+  // LEAFLET Settings
+  if (navigator.platform === 'iPhone') {
+    L_DISABLE_3D = true;
+  }
+}())
